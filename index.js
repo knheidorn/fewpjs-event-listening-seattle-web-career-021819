28c825805db8ccac6347888c5ef0e2f77b1c6a0a
Kimberly Heidorn <heidorkn@gmail.com>
2019-04-01 16:36:43 -0700

    walkthrough with alert coding

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-function addingEventListener() {
+const main = document.getElementById('input');
+
+function addingEventListener(){
+  main.addEventListener('click', function(event) {
+    alert ('I was clicked!');
+  });
 }
